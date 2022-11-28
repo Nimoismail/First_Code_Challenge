54422a26240a184c54829716b2bc2b475368c191
nimo <nimo.sheikh@student.moringaschool.com>
2022-11-28 14:55:23 +0300

    enterd studen marks and graded according to their grade

---
+++ speedDetector.js
@@ -0,0 +1,20 @@
+//Challenge 2: Speed Detector (Toy Problem)
+//here we calculate speed of a car 
+//lets speed of a car is 80;
+function speedOfAcar(){
+let speedOfAcar = 80;
+let demeritPointsOfCar = (speedOfAcar - 70)/5;
+//using if statemnt..if speedOfAcar is less than 70; we  print "Ok";
+if(speedOfAcar<70){
+    console.log("Ok");
+  }
+  //otherwise we prints points if points exceeds 12,we print liscences suspended..for every 5 km/hr above speed limit 70,
+  else {
+    console.log("demeritPoint:" + demeritPointsOfCar);
+  //if the points exceeds 12 we print licences suspended
+   if (demeritPointsOfCar > 12 ){
+  console.log("licence suspended");
+  }
+}
+}
+speedOfAcar();
---
+++ studentGrade.js
@@ -0,0 +1,23 @@
+//Challenge 1: Student Grade Generator (Toy Problem)
+//At first we prompt the student marks
+let marks = prompt("Enter Student Marks");
+//the marks between 79 to 100 by using if statement we console.log("A")
+if (marks > 79 && marks <=100){
+    console.log("A");
+}
+//else if the marks is less than between 79 and 60;we console.log grade "B";
+else if(marks >= 60 && marks <=79){
+    console.log("B");
+}
+//else if the marks is in between 49 and 59;we console.log("C")
+else if (marks >= 49 && marks <= 59){
+   console.log("C");
+}
+// else if the marks is between 40 and 49;we console.log"D"
+else if (marks >= 40 && marks <=49){
+    console.log("D")
+}
+//else if the marks is less than 40 percent we console.log"E
+else if (marks > 40) {
+    console.log("E")
+  }
